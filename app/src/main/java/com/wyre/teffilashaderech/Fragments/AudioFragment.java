75c1ecb4d9290f8610af66b3043a4929fb579c8c
greatjack1 <yaakovrim@gmail.com>
2018-11-14 15:05:47 -0500

    Improved audio Feature, fixed bugs releated to media player state

--- app/src/main/java/com/wyre/teffilashaderech/Fragments/AudioFragment.java
+++ app/src/main/java/com/wyre/teffilashaderech/Fragments/AudioFragment.java
@@ -5,18 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 
 import com.wyre.teffilashaderech.R;
@@ -26,21 +23,19 @@ import java.util.TimerTask;
 
 
 public class AudioFragment extends Fragment {
-    private Button mBtnPlay;
-    private Button mBtnStop;
+    private ImageButton mBtnPlay;
     private Toolbar mToolbar;
     private SeekBar mSeekbar;
     private MediaPlayer mPlayer;
     private Timer mTimer;
     private boolean mTimerRunning = false;
-
+    private boolean mPlaying = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setRetainInstance(true);
         View v = inflater.inflate(R.layout.audio_fragment, container, false);
         mBtnPlay = v.findViewById(R.id.btn_play);
-        mBtnStop = v.findViewById(R.id.btn_stop);
         mSeekbar = v.findViewById(R.id.seekBar);
         //set the support action bar in the fragment
         AppCompatActivity act = (AppCompatActivity) getActivity();
@@ -70,42 +65,50 @@ public class AudioFragment extends Fragment {
             }
         });
         mBtnPlay.setOnClickListener((n) -> {
-            //intialize the media player if its null
-            if (mPlayer == null) {
-                mPlayer = MediaPlayer.create(getContext(), R.raw.teffilashaderech);
-            }
-            mPlayer.start();
-            mSeekbar.setMax(mPlayer.getDuration());
-            //Create a timer that update the seekbar with the media players progress
-            if (mTimerRunning == false) {
-                mTimerRunning = true;
-                TimerTask task = new TimerTask() {
-                    @Override
-                    public void run() {
-                        getActivity().runOnUiThread((new Runnable() {
-                            @Override
-                            public void run() {
-                                if (mPlayer != null) {
-                                    if (mPlayer.isPlaying()) {
-                                        mSeekbar.setProgress(mPlayer
-                                                .getCurrentPosition());
+            //if its not playing then start the player
+            if(!mPlaying) {
+                mPlaying = true;
+                mBtnPlay.setImageResource(R.drawable.ic_stop_black_24dp);
+                //intialize the media player if its null
+                if (mPlayer == null) {
+                    mPlayer = MediaPlayer.create(getContext(), R.raw.teffilashaderech);
+                }
+                mPlayer.start();
+                //switch over the image of t
+                mSeekbar.setMax(mPlayer.getDuration());
+                //Create a timer that update the seekbar with the media players progress
+                if (mTimerRunning == false) {
+                    mTimerRunning = true;
+                    TimerTask task = new TimerTask() {
+                        @Override
+                        public void run() {
+                            getActivity().runOnUiThread((new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (mPlayer != null) {
+                                        if (mPlayer.isPlaying()) {
+                                            mSeekbar.setProgress(mPlayer
+                                                    .getCurrentPosition());
+                                        }
+                                    } else {
+                                        //since its null set the seekbar progress to 0
+                                        mSeekbar.setProgress(0);
                                     }
-                                } else {
-                                    //since its null set the seekbar progress to 0
-                                    mSeekbar.setProgress(0);
                                 }
-                            }
-                        }));
-                    }
-                };
-                mTimer = new Timer();
-                mTimer.schedule(task, 1000, 1000);
+                            }));
+                        }
+                    };
+                    mTimer = new Timer();
+                    mTimer.schedule(task, 1000, 1000);
+                }
             }
-        });
-        mBtnStop.setOnClickListener((n) -> {
-            mPlayer.pause();
-            mPlayer.seekTo(0);
-            mSeekbar.setProgress(0);
+            else {
+                mPlaying = false;
+                mBtnPlay.setImageResource(R.drawable.ic_play);
+                mPlayer.pause();
+                mPlayer.seekTo(0);
+                mSeekbar.setProgress(0);
+            }
         });
         return v;
     }
